4ef846f648d1592d4f958f37d631f49a03ca60c8
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-11-21 11:55:50 -0600

    enable-migration, add-migration, update-database complete.

---
+++ SuperheroCreatorWebApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace SuperheroCreatorWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SuperheroCreatorWebApp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SuperheroCreatorWebApp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
