1f61f40405db7a73abef25e3972b2b69fe223c5b
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-11-21 11:36:49 -0600

    Init commit

---
+++ SuperheroCreatorWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperheroCreatorWebApp.MvcApplication" Language="C#" %>
---
+++ SuperheroCreatorWebApp/Models/Superhero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperheroCreatorWebApp.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string alterEgo { get; set; }
+        public string primarySuperheroAbility { get; set; }
+        public string secondarySuperheroAbility { get; set; }
+        public string catchphrase { get; set; }
+    }
+}
