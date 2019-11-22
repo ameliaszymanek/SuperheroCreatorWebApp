9a2c31648549f5659e7abc65814628dc947ee997
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-11-22 09:29:20 -0600

    Added new bootsrap theme successfully

--- SuperheroCreatorWebApp/App_Start/RouteConfig.cs
+++ SuperheroCreatorWebApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SuperheroCreatorWebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Superhero", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
